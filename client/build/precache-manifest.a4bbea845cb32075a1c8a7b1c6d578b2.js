04f56c16bdf10237f20dc758829850b938700bbc
Marshallb98 <69322012+Marshallb98@users.noreply.github.com>
2021-03-18 22:28:38 -0500

    Build

---
+++ client/build/precache-manifest.a4bbea845cb32075a1c8a7b1c6d578b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96db61a3195298abebc94463c6e79e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43038ed8d612303f2e47",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43038ed8d612303f2e47",
+    "url": "/static/js/main.83b86d15.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
